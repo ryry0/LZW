6be1696c537f2809d4c5f4a91fbedcae37e550c6
Ryan Reyes <ryan.david.reyes@gmail.com>
2016-04-19 18:49:36 -0400

    Working compression algorithm.
    Hooooray.

--- compress.c
+++ compress.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdint.h>
 #include <ring_buffer.h>
 #include <hashtable.h>
 
-#define NUM_BUCKETS 10
+#define NUM_BUCKETS 6000
 #define BUFFER_SIZE 3841 //from 2^12 - 2^8 +1
+
+union ben {
+  uint16_t num;
+  char c[2];
+};
+
+int fpeek(FILE *file) {
+  int c;
+  c = fgetc(file);
+  ungetc(c, file);
+  return c;
+}
+
+void initDictionary(hash_table *h_table) {
+  for (uint16_t i = 0; i < 256; ++i) {
+    char c[2] = {0};
+    c[0] = i;
+    insert(h_table, c, i);
+  }
+}
 
 int main(int argc, char ** argv) {
 
-  int input_fd;
-  int output_fd;
+  FILE *input_fp;
+  FILE *output_fp;
 
   int key = 256;
-  hash_node* last_match = NULL;
-
-  struct ring_buffer input_buffer;
   hash_table dictionary;
 
-  char current;
+  char current_char;
 
   if (argc > 2) {
-    input_fd = open(argv[1], O_RDONLY);
-    if (input_fd == -1) {
+    input_fp = fopen(argv[1], "rb");
+    if (input_fp == NULL) {
       printf("File not found!");
       return 1;
     }
-    output_fd = open(argv[2], O_TRUNC |O_RDWR | O_CREAT, 0666);
+    output_fp = fopen(argv[2], "wb");
   }
   else {
     printf("Please provide filenames for");
@@ -37,62 +52,44 @@ int main(int argc, char ** argv) {
   }
 
   //init data structures
-  initBuffer(&input_buffer, BUFFER_SIZE);
   initTable(&dictionary, NUM_BUCKETS);
+  initDictionary(&dictionary);
 
-  //read from file
+  char input_buffer[BUFFER_SIZE] = {0};
+  size_t buffer_end = 0;
+  while (fread(&current_char, 1, 1, input_fp)) {
+    char lookup_str[BUFFER_SIZE] = {0};
+    //peek the next byte
+    int peeked_val = fpeek(input_fp);
 
-  //priming read
-  read(input_fd, &current, 1);
-  pushBack(&input_buffer, current);
 
-  while (read(input_fd, &current, 1)) {
-    char string_buffer[BUFFER_SIZE] = {0};
-    char output_buffer[BUFFER_SIZE] = {0};
-    size_t write_len = 0;
-
-    printf("current character read: %c\n", current);
-    if (current == EOF)
-      break;
-
-    pushBack(&input_buffer, current);
-
-    //get string inside input_buffer
-    readBuffer(&input_buffer, string_buffer);
-    printf("string buffer before retrieve: %s\n", string_buffer);
+    input_buffer[buffer_end++] = current_char;
+    strcpy(lookup_str, input_buffer);
+    lookup_str[buffer_end] = peeked_val;
 
     hash_node * node = NULL;
-    if ((node = retrieve(&dictionary, string_buffer)) != NULL) {
-      last_match = node;
-      continue;
-    }
+    if (((node = retrieve(&dictionary, input_buffer)) != NULL) &&
+      ((retrieve(&dictionary, lookup_str) == NULL) ||
+      (peeked_val == EOF))) {
 
-    if (key < 4096)
-      insert(&dictionary, string_buffer, key++);
+      union ben b;
+      b.num = node->data;
+      fwrite(b.c, 1, 2, output_fp);
 
+      if (key < 60000) {
+        insert(&dictionary, lookup_str, key++);
+      } else {
+        printf("Dictionary full, no entry will be made for %s",
+            lookup_str);
+      } //end else
+      memset(input_buffer, 0, BUFFER_SIZE); //clear buffer
+      buffer_end = 0;
+    } //end if
+  } //end while
 
-    write_len = input_buffer.length - 1;
-    popAllButBack(&input_buffer, output_buffer);
+  fclose (input_fp);
+  fclose (output_fp);
 
-    if (last_match == NULL) {
-      write(output_fd, output_buffer, write_len);
-      printf("output_buffer: %s\n", output_buffer);
-    }
-    else {
-
-      sprintf(output_buffer, "%d", last_match->data);
-      write(output_fd, output_buffer, strlen(output_buffer));
-      /*
-      write(output_fd, (char *) &itoa(last_match->data), sizeof(int));
-      */
-      printf("output_buffer(int): %d\n", last_match->data);
-    }
-  }
-
-  close (input_fd);
-  close (output_fd);
-
-  destroyBuffer(&input_buffer);
   destroyTable(&dictionary);
 
   return 0;
